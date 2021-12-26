e40b32964981563422e1fdc39565b5ea70a18d36
greenboum-l <leahg8781@gmail.com>
2021-12-26 17:07:51 +0200

    addFilter

---
+++ src/FilterBorrowedBooks.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+export const  FilterBorrowedBooks=() =>{
+  return (
+    <FormGroup>
+      <FormControlLabel control={<Switch defaultChecked />} label="Get only free books" />
+    </FormGroup>
+  );
+}
+export default FilterBorrowedBooks;
