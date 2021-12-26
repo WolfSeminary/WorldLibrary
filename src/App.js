dbfabf39e074596618c13eaa15da96734577153f
sariYagelnik <sariYagelnik@gmail.com>
2021-12-26 19:18:52 +0200

    add library status button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import BooksStatus from './BookStatus';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
+    {/* //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload. Deploy-work!
+    //     </p>
+    //   </header> */}
+      <BooksStatus/>
     </div>
   );
 }
