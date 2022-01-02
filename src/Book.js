82f1734f8f6d1728c678df10853d1d3f7970a015
EstiAkerman <hitmachut@students.local>
2022-01-02 16:48:29 +0200

    booksComponent

---
+++ src/Book.js
@@ -0,0 +1,54 @@
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function FormRow(props) {
+const  arrBooks=props.arrBooks;
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>arrBooks[0]</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>arrBooks[1]</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>arrBooks[2]</Item>
+      </Grid>
+    </React.Fragment>
+  );
+}
+export default function Books(props) {
+ const allBooks = props.books;
+  let sliceArrBook;
+  let i = 0;
+  return (
+
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        {allBooks.map((book, index) => {
+         sliceArrBook = allBooks.slice(i, 3);
+         i+=3;
+          return (
+        
+          <Grid container item spacing={3}>
+            <FormRow arrBooks={sliceArrBook}/>
+          </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
+}
+
+
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Books from './Book';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Books />
   </React.StrictMode>,
   document.getElementById('root')
 );
