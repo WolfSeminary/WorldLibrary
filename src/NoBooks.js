141afba280509a23f2fad9aaa2610e537594fa16
avigailbarron <avigail0533166501@gmail.con>
2022-01-02 19:16:17 +0200

    AddANoBooksComponent

---
+++ src/NoBooks.js
@@ -0,0 +1,13 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import "@fontsource/roboto"
+import * as React from 'react';
+import { Typography } from '@mui/material';
+export default function NoBooks() {
+  return <Typography id="typography" variant="right" component="h2">
+    "Sorry, no available books"
+  </Typography>
+}
+
