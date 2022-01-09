cc09ec529c49ea46dc5a85c0b7027fe41be65630
avigailbarron <96010117+avigailbarron@users.noreply.github.com>
2022-01-09 14:36:22 +0200

    Add Search Bar Component (#40)
    * Add Search Bar Component
    
    * fix
    
    Co-authored-by: Ora Benesh <orabenesh@gmail.com>

---
+++ src/AppBarCompoent.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+
+export default function AppBarCompoent() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ display: { xs: 'none', sm: 'block' } }}
+                    >
+                        library
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
