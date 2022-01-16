50d2c57f89d44aa10bdbab81041c3129ee8ef926
shifiAvruzhki <s4114008@gmail.com>
2022-01-16 17:16:17 +0200

    update book state

--- src/FilterBorrowedBooks.js
+++ src/FilterBorrowedBooks.js
@@ -4,13 +4,13 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
 export const FilterBorrowedBooks = () => {
-const [bookStatus,setBookStatus]=useState("free");
- const onStatusChange=()=>{
-  bookStatus=="free"?setBookStatus("borrowed"):setBookStatus("free");
+  const [bookStatus, setBookStatus] = useState("free");
+  const onStatusChange = () => {
+    bookStatus == "free" ? setBookStatus("borrowed") : setBookStatus("free");
   }
   return (
     <FormGroup>
-      <FormControlLabel control={<Switch  onChange={onStatusChange} />} label="Get only free books" />
+      <FormControlLabel control={<Switch onChange={onStatusChange} />} label="Get only free books" />
     </FormGroup>
   );
 }
