7121e3153960c893af65638709aee387dd897206
RuthiGruner <96010364+RuthiGruner@users.noreply.github.com>
2022-01-25 18:59:56 +0200

    NoBooks component - Render (#60)
    Co-authored-by: RuthiGruner <esterv9413@gmail.com>
    Co-authored-by: orabenesh <orabenesh@gmail.com>

--- src/Books.js
+++ src/Books.js
@@ -1,6 +1,34 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Book from './Book';
+import { NoBooks } from 'react';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+function FormRow(props) {
+  const arrBooks = props.arrBooks;
+  return (
+   
+    <React.Fragment>
+       {books==null &&<NoBooks/>}
+      <Grid item xs={4}>
+        <Item>arrBooks[0]</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>arrBooks[1]</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>arrBooks[2]</Item>
+      </Grid>
+    </React.Fragment>
+  );
+}
 
 export default function Books() {
   const [books, setBooks] = useState([])
