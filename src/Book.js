f5502ca48853cf4cadb6b46e0f97cc78f7e1acc3
greenboum-l <96010340+greenboum-l@users.noreply.github.com>
2022-01-27 19:29:53 +0200

    hhhh (#61)
    Co-authored-by: orabenesh <orabenesh@gmail.com>

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, useThemeProps } from '@mui/material';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import { useNavigate } from "react-router-dom";
 
 export default function ActionAreaCard(props) {
+<<<<<<< HEAD
+  const [isFree, setIsFree] = useState(props.book.status === "free")
+
+  const onStatusChange = () => {
+    ;
+    props.book.status === "free" ? props.book.status = "borrowed" : props.book.status = "free";
+    props.bookStatusChange(props.book);
+  }
+=======
   const navigate = useNavigate();
   function onBookClick() {
     return (
@@ -17,8 +26,11 @@ export default function ActionAreaCard(props) {
     );
   }
   const [isFree, setIsFree] = React.useState(true)
+>>>>>>> origin/master
   const handleChange = (event) => {
+    ;
     setIsFree(event.target.checked);
+    onStatusChange();
   }
   return (
     <Card onClick={onBookClick} sx={{ maxWidth: 345 }}>
@@ -26,24 +38,29 @@ export default function ActionAreaCard(props) {
         <CardMedia
           component="img"
           height="140"
+<<<<<<< HEAD
+          // image={props.book.volumeInfo.imageLinks.thumbnail}
+          alt={props.book.volumeInfo && props.book.volumeInfo.title}
+=======
           image={
             book.volumeInfo.imageLinks === undefined
               ? ""
               : `${book.volumeInfo.imageLinks.thumbnail}`
         }
           alt="green iguana"
+>>>>>>> origin/master
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {book.BookName}
+            {props.book.BookName}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {book.BookInfo}
+            {props.book.BookInfo}
           </Typography>
         </CardContent>
       </CardActionArea>
       <FormGroup>
-        <FormControlLabel control={<Switch checked={isFree}
+        <FormControlLabel control={<Switch checked={isFree} 
           onChange={handleChange} />} label={isFree ? "free" : "borrowed"} />
       </FormGroup>
     </Card>
